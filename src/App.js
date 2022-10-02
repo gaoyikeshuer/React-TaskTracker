83067eb4b398cebc413103f5ec8cf91bf4d3d9e1
Yike Gao <yikegao8@gmail.com>
2022-10-02 10:54:02 +0100

    10.2

--- src/App.js
+++ src/App.js
@@ -1,61 +1,88 @@
-import { useState,useEffect } from 'react';
-import './App.css';
-import Header from './components/header/Header';
-import Tasks from './components/tasks/Tasks';
-import AddTask from './components/AddTask/AddTask';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Tasks from "./components/tasks/Tasks";
+import AddTask from "./components/AddTask/AddTask";
 
 function App() {
-  const[showAddTask,setShowAddTask] = useState(false)
-  const[tasks,setTasks] = useState();
- useEffect(()=>{
-  const getTasks = async () =>{
-    const tasksFromServer = await fetchTasks()
-    setTasks(tasksFromServer)
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState('');//must have sth for useState
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+  //Fetch datas
+  const fetchTasks = async () => {
+    const res = await fetch(`http://localhost:5000/tasks`);
+    const data = await res.json();
+    console.log(data)
+    return data;
+  };
+  //Fetch selected task
+  const fetchTask = async (id) =>{
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
   }
-  getTasks()
- 
-
- },[])
- //Fetch data
- const fetchTasks = async() =>{
-  const res = await fetch('http://localhost:5000/tasks')
-  const data = await res.json()
-  return data
-}
 
   //delete task
 
-  const deleteTask =(id) =>{
-    setTasks(tasks.filter((task)=>(task.id !== id)))//new array
-  }
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+    setTasks(tasks.filter((task) => task.id !== id)); //new array
+  };
   //Toggle Reminder
 
-  const toggleReminder = (id) =>{
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder:!taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+    const data = await res.json()
     setTasks(
-      tasks.map((task) =>(
-        task.id === id ?
-        {...task, reminder:!task.reminder}:task
-      ))//spread operator
-    )
-
-  }
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      ) //spread operator  UI 
+    );
+  };
   //AddTask
 
-  const addTask = (task) =>{
-    const id = Math.floor(Math.random()*1000)+1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: { //must be headers? why
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(task),//??
+    });
+    const data = await res.json();
+    setTasks([...tasks, data]);
 
-  }
+    // const id = Math.floor(Math.random()*1000)+1
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
+  };
 
   return (
     <div className="App">
-     <Header title='Hello' onAdd = {()=>setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
-     {showAddTask && <AddTask onAdd ={addTask}/>}
-     {tasks.length>0?<Tasks tasks={tasks} onDelete ={deleteTask} onToggle = {toggleReminder}/> :('No Tasks to Show' )}
+      <Header
+        title="Hello"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length>0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : ("No Tasks to Show")}
     </div>
-
   );
 }
 
--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -4,7 +4,7 @@ import './Task.css'
 
 const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+    <div className={`task ${task.reminder?'reminder':''}`} onDoubleClick={() => onToggle(task.id)}>
         <h3>{task.text} <FaTimes style={{cursor:'pointer'}} onClick = {() => onDelete(task.id)}/></h3>
         <p>{task.day}</p>
     </div>
